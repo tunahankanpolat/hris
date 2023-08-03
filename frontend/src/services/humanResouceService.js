de92d07e375a01248063307d86d5830101b2b106
Tunahan Kanpolat <kanpolatunahan@gmail.com>
2023-08-04 02:49:19 +0300

    # Backend frontend connections

--- frontend/src/services/humanResouceService.js
+++ frontend/src/services/humanResouceService.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 export default class HumanResourceService {
-  login(userName, password) {
-    return axios.post(process.env.REACT_APP_HUMAN_RESOURCE_LOGIN_URL, {
+  async login(userName, password) {
+    return await axios.post(process.env.REACT_APP_HUMAN_RESOURCE_LOGIN_URL, {
       userName: userName,
       password: password,
     });
   }
-  getJobPosts(token, page, size) {
-    return axios.get(
+  async getJobPosts(token, page, size) {
+    return await axios.get(
       process.env.REACT_APP_HUMAN_RESOURCE_JOB_POSTS_URL + `/${page}/${size}`,
       {
         headers: {
@@ -16,4 +16,15 @@ export default class HumanResourceService {
       }
     );
   }
+  async getJobPostApplications(token, jobPostId, page, size, status) {
+    return await axios.get(
+      process.env.REACT_APP_HUMAN_RESOURCE_JOB_POSTS_URL + `/${jobPostId}/jobApplications/${page}/${size}` + (status ? `?jobApplicationStatus=${status}` : ""),
+      {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      }
+    );
+  }
+
 }
